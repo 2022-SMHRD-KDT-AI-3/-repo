d5cd1d17aba5efce517717fc9462082712749836
안인성 <ahn4943@naver.com>
2022-04-08 14:03:29 +0900

    today최종본 적용완료

--- project_인성/router/router.js
+++ project_인성/router/router.js
@@ -444,7 +444,7 @@ router.get("/sportsboard", function(request, response){
 
 router.get("/lifeboard", function(request, response){
        
-    let sql = "select * from board where text_cg = '생활문화'";
+    let sql = "select * from board where text_cg = '생활/문화'";
     
     conn.query(sql, function(err, rows){        
         if(rows){
